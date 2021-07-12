51f01e1069931048370defb85adac3a3cb43f6dd
izping <60201426+izping@users.noreply.github.com>
2021-07-12 12:30:16 -0500

    Create 50-before-post-start.sh

---
+++ baseline/pingfederate/hooks/50-before-post-start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+#
+# Ping Identity DevOps - Docker Build Hooks
+#
+#- This is called after the start or restart sequence has finished and before 
+#- the server within the container starts
+#
+
+echo PingID IK
+# PingID IK
+rm -f /opt/out/instance/server/default/deploy/pf-pingid-idp-adapter-2.10.jar
