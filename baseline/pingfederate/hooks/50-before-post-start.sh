905388646fca73bca0680b3f64b64a94f27ca4c5
izping <60201426+izping@users.noreply.github.com>
2021-07-12 17:18:32 -0500

    Update 50-before-post-start.sh

--- baseline/pingfederate/hooks/50-before-post-start.sh
+++ baseline/pingfederate/hooks/50-before-post-start.sh
@@ -6,6 +6,8 @@
 #- the server within the container starts
 #
 
+echo Hello from the server profile 50-before-post-start.sh hook!
+
 echo PingID IK
 # PingID IK
 rm -f /opt/out/instance/server/default/deploy/pf-pingid-idp-adapter-2.10.jar
